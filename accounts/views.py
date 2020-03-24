b977322d1f3d4a194849d14df50b99195aefe3a9
RobSimons1 <rob.simons79@gmail.com>
2020-03-24 12:44:17 +0000

    Form validation setup in progress

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
-from accounts.forms import UserLoginForm
+from accounts.forms import UserLoginForm, UserRegistrationForm
+
 
 def index(request):
     """Return the index.html file"""
@@ -26,12 +27,19 @@ def login(request):
         if login_form.is_valid():
             user = auth.authenticate(username=request.POST['username'],
                                     password=request.POST['password'])
+            messages.success(request, "You have successfully logged in!")
+
             if user:
                 auth.login(user=user, request=request)
-                messages.success(request, "You have successfully logged in!")
                 return redirect(reverse('index'))
             else:
                 login_form.add_error(None, "Your username or password is incorrect")
     else:
         login_form = UserLoginForm()
     return render(request, 'login.html', {'login_form': login_form})
+
+def registration(request):
+    """Render the registration page"""
+    registration_form = UserRegistrationForm()
+    return render(request, 'registration.html', {
+        "registration_form": registration_form})
